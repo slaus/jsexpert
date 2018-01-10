a3a7fc4bf8dca0a2d4801bba39cae4208c8b2ced
slaus <mister.slaus@gmail.com>
2018-01-10 17:31:31 +0200

    Update script.js

--- homework-13-3/js/script.js
+++ homework-13-3/js/script.js
@@ -1,36 +1,36 @@
-'use strict';
+"use strict";
 
 var result = "";
 const elem = document.getElementById("result");
 // elem.innerHTML = result;
 
-var num = +prompt('Сколько ворон сидит на ветке?');
+var num = +prompt("Сколько ворон сидит на ветке?");
 
-
-function numberOfCrows(num) {
-  if(num !== null) {
-    console.log(num);
-    if(num.substr(-1) == 1) {
-      console.log(num.substr(-1));
-      result = "На ветке сидит " + num + " ворона."
-
+if (!isNaN(num)) {
+    if (num < 11 && num > 19) {
+        result = "На ветке сидит " + num + " ворон.";
+    } else if (num.toString().substr(-1) == 1) {
+        result = "На ветке сидит " + num + " ворона.";
+    } else if (
+        num.toString().substr(-1) == 2 ||
+        num.toString().substr(-1) == 3 ||
+        num.toString().substr(-1) == 4
+    ) {
+        result = "На ветке сидит " + num + " вороны.";
+    } else {
+        result = "На ветке сидит " + num + " ворон.";
     }
-  } else {
+} else {
     alert("Ошибка. Введите число!");
-  }
 }
 
-function result(string) {
-  result += string;
-  elem.innerHTML = result;
-}
+elem.innerHTML = result;
 
-function isLeapYear () {
-  
-}
+function numberOfCrows() {}
+
+function isLeapYear() {}
 
 //console.log(isLeapYear(1600));  //true
 //console.log(isLeapYear(2100));  //false
 //console.log(isLeapYear(2012)); //true
 //console.log(isLeapYear());    //Error - year is not found
-
